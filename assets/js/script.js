f64100aae6339c8df1db718f5883feadf94d9fca
kanzansio <eduardoakan@gmail.com>
2021-04-29 03:03:17 -0500

    solucion con js

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,4 +143,42 @@ function zoomIn(){
   removeClass(site, 'show-all');
 }
 
-
+// Functions central Div
+function centrarDiv() {
+  var wHeight = 0;
+  var wWidth = 0;
+  var dHeight = 0;
+  var dWidth = 0;
+ 
+  if (typeof(window.innerWidth) == 'number') {
+    // Navegadores NO Internet Explorer
+    wWidth = window.innerWidth;
+    wHeight = window.innerHeight;
+  } else if (document.documentElement &&
+        (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+    // Navegadores Internet Explorer desde la versión 6.0 en modo de compilación estándar
+    wWidth = document.documentElement.clientWidth;
+    wHeight = document.documentElement.clientHeight;
+  } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+    // Navegadores Internet Explorer 4
+    wWidth = document.body.clientWidth;
+    wHeight = document.body.clientHeight;
+  }
+ 
+  var central = document.getElementById('central');
+  dHeight = central.offsetHeight;
+  dWidth = central.offsetWidth;
+ 
+  var top = (wHeight - dHeight) / 1.7;
+  var left = (wWidth - dWidth) / 2;
+ 
+  if (top < 0) {
+    top = 0;
+  }
+  if (left < 0) {
+    left = 0;
+  }
+ 
+  central.style.top = top + 'px';
+  central.style.left = left + 'px';
+}
